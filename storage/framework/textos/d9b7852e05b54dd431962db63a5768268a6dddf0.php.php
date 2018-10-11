28ddffd0d25a686db7cd91b7ebc0ee128feee461
Unai <unai@irunweb.com>
2018-10-11 14:58:44 +0200

    po edit create

--- storage/framework/textos/d9b7852e05b54dd431962db63a5768268a6dddf0.php.php
+++
@@ -1,7 +0,0 @@
-<?php if(count($breadcrumbs)): ?>
-    <ol class="breadcrumb">
-        <?php $__currentLoopData = $breadcrumbs; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $breadcrumb): $__env->incrementLoopIndices(); $loop = $__env->getFirstLoop(); ?>
-                <li class="breadcrumb-item"><a href="<?php echo e($breadcrumb->url); ?>"><?php echo e($breadcrumb->title); ?></a></li>
-        <?php endforeach; $__env->popLoop(); $loop = $__env->getFirstLoop(); ?>
-    </ol>
-<?php endif; ?>
